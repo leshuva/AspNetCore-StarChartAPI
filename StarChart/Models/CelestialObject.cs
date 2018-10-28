5d0e8a2606d71b07042ae3d8fcdcbd0fe2519c55
leshuva <moshele1@outlook.com>
2018-10-28 13:25:06 +1300

    Module completed

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -13,7 +13,7 @@ namespace StarChart.Models
         [Required]
         public string Name { get; set; }
 
-        public int? OrbitedId { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
